d666c9d34d19447b059ae200ee02c6fb78679ee7
Cesar Anacoreta <cesaranacoreta29@gmail.com>
2023-02-01 14:06:51 -0600

    SOlucion de primeros 2 ejercicios del PrepCourse

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,7 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +12,36 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var resultado = (x+y);
+   return resultado;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var resResta = (x-y);
+   return resResta;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   var resDivide = (x / y);
+   return resDivide;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var mulResul = (x * y);
+   return mulResul;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var restoR = (x % y);
+   return restoR;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
